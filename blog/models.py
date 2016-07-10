2be92fb8bcc31ce125eb394c57b4fb613b9287b0
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-10 12:18:26 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,23 @@ class Product(models.Model):
     name = models.CharField(max_length= 254, default = '')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    @property
+    def paypal_form(self):
+        paypal_dict = {
+            'business' : settings.PAYPAL_RECEIVER_EMAIL,
+            'amount': self.price,
+            'currency_code': 'EUR',
+            'item_name': self.name,
+            'img_name': self.img,
+            'invoice': '%s-%s' % (self.pk, uuid.uuid4()),
+            'notify_url': settings.PAYPAL_NOTIFY_URL,
+            'return_url':'%s/paypal-return/' % settings.SITE_URL,
+            'cancel_url':'%s/paypal-cancel/' % settings.SITE_URL
+        }
+        return PayPalPaymentsForm(initial=paypal_dict)
+
+
 
 
 class Post(models.Model):
