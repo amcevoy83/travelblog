7f73b381df1bc282cfc39e73abda1346b8b51e54
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-10 12:48:11 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns =[
               # url(r'^blog/$', views.post_list, name='post_list'),
-              url(r'^$', views.new_product, name='new{product'),
+              url(r'^$', views.home, name='home'),
               url(r'^blog/(?P<id>\d+)/$', views.post_detail),
               url(r'^blog/$', views.melbourne,  name='melbourne'),
               url(r'^post/new/$', views.new_post, name='new_post'),
               url(r'^gallery/$', views.gallery, name='gallery'),
               url(r'^suggestion/$', views.suggestion_post, name='suggestion_post'),
-              url(r'^test/', views.new_product, name='new_product'),
+              url(r'^test/$', views.new_product, name='new_product'),
 
 ]
 
