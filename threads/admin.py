0b5c03772b633a8622f54b4af4296185f6dfe166
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-10 17:17:35 +0100

    Delete admin.py

--- threads/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Thread, Subject, Posts
-
-# Register your models here.
-
-admin.site.register(Thread)
-admin.site.register(Posts)
-admin.site.register(Subject)
