3fc8dbb4881ae87bc0bfad5e19c864ad338d9a2d
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-10 14:31:14 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns =[
               # url(r'^blog/$', views.post_list, name='post_list'),
               url(r'^$', views.home, name='home'),
               url(r'^blog/(?P<id>\d+)/$', views.post_detail),
-              url(r'^blog/$', views.home,  name='home'),
+              url(r'^blog/$', views.post_list,  name='post_list'),
               url(r'^post/new/$', views.new_post, name='new_post'),
               url(r'^gallery/$', views.gallery, name='gallery'),
               url(r'^suggestion/$', views.suggestion_post, name='suggestion_post'),
