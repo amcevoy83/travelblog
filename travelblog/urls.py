0b1873667325e60b1c91241fd1d9b11487b510da
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-10 16:13:03 +0100

    Update urls.py

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -1,4 +1,4 @@
-"""travelblog URL Configuration
+"""Bootcamp_Blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -15,20 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT
 from blog.views import *
-from settings import MEDIA_ROOT
-import settings
-from django.conf.urls.static import static
-#from views import paypal_return, paypal_cancel, new_product
-from paypal.standard.ipn import urls as paypal_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^p4yp4lur14u5e/', include(paypal_urls)),
-    url(r'^paypal-return/', paypal_return),
-    url(r'^product/$', new_product, name='new_product'),
-    url(r'^paypal-cancel/', paypal_cancel),
     url(r'',include('blog.urls')),
     url(r'^post/new/$',new_post, name='new_post'),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': MEDIA_ROOT})
+]
