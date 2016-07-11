495324c637282e2eabed5e74bcdda4b25f75ab4b
amcevoy83 <aoifemcevoy@gmail.com>
2016-07-11 22:19:37 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 import views
 from . import views
-from accounts.views import *
 
 urlpatterns =[
               # url(r'^blog/$', views.post_list, name='post_list'),
@@ -11,10 +10,6 @@ urlpatterns =[
               url(r'^post/new/$', views.new_post, name='new_post'),
               url(r'^gallery/$', views.gallery, name='gallery'),
               url(r'^suggestion/$', views.suggestion_post, name='suggestion_post'),
-              url(r'^register/$','accounts.views.register', name='register'),
-              url(r'^login/$','accounts.views.login', name='login'),
-              url(r'^logout/$','accounts.views.logout', name='logout'), ##removed two // at the start of the logout regex
-              url(r'^profile/$', 'accounts.views.profile', name='profile'),
             #  url(r'^post/new/$', views.new_post, name='new_post'),
 
 ]
